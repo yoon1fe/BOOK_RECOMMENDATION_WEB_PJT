f6c17dff1a193f0859004265269ba84b2b0ed329
yoon1fe <yoon1fe@naver.com>
2020-06-03 14:49:53 +0900

    브랜치나누고 Board Comment DTO DAO 생성

--- src/com/dao/CommentDAO.java
+++ src/com/dao/CommentDAO.java
@@ -1,9 +1,8 @@
 package com.dao;
 
-public class BoardDAO {
+public class CommentDAO {
 	private static String dburl = "jdbc:mysql://15.165.219.175:3306/webPjt?serverTimezone=UTC";
 	private static String dbUser = "connectuser";
 	private static String dbPw = "connect123!@#";
-	
 	
 }
---
+++ src/com/dto/CommentDTO.java
@@ -0,0 +1,43 @@
+package com.dto;
+
+public class CommentDTO {
+	private int comment_number;
+	private String id;
+	private int board_number;
+	private String comment_content;
+	
+	
+	public CommentDTO(int comment_number, int board_number, String comment_content) {
+		super();
+		this.comment_number = comment_number;
+		this.board_number = board_number;
+		this.comment_content = comment_content;
+	}
+	
+	public int getComment_number() {
+		return comment_number;
+	}
+	public void setComment_number(int comment_number) {
+		this.comment_number = comment_number;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getBoard_number() {
+		return board_number;
+	}
+	public void setBoard_number(int board_number) {
+		this.board_number = board_number;
+	}
+	public String getComment_content() {
+		return comment_content;
+	}
+	public void setComment_content(String comment_content) {
+		this.comment_content = comment_content;
+	}
+	
+	
+}
