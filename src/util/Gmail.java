a00960eac452d22c30457d65ce68731d0ea26220
yoon1fe <yoon1fe@naver.com>
2020-07-02 06:43:29 +0900

    진짜 최종

---
+++ src/util/Gmail.java
@@ -0,0 +1,25 @@
+package util;
+
+
+
+import javax.mail.Authenticator;
+
+import javax.mail.PasswordAuthentication;
+
+
+
+public class Gmail extends Authenticator {
+
+
+
+    @Override
+
+    protected PasswordAuthentication getPasswordAuthentication() {
+
+        return new PasswordAuthentication("gmailID","password");
+    }
+
+    
+
+}
+
